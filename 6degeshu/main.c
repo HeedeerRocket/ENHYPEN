593d698ed6b054f7aa3f10d1ae93cd170c3c1ed7
Heedeer <157211513+HeedeerRocket@users.noreply.github.com>
2024-01-21 09:46:40 +0800

    Add files via upload

---
+++ 6degeshu/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+
+
+
+
+
+int jisuan(int n,int a[10000])
+{
+    for(int i=0;i<n;i++)
+    {
+
+    }
+}
